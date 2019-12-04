cbe9a1fc016be91410fb5f1fc26c2e211aec83d2
Daniel Rooker <dannyrooks@gmail.com>
2019-12-04 18:38:50 -0500

    created models

--- db/migrate/20191204222154_create_users.rb
+++ db/migrate/20191204222154_create_users.rb
@@ -1,9 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.text :body
-      t.integer :user_id
+      t.string :email
     end
   end
 end
